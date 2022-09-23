e1741c323e824ec8748afb86a50f2a829feeec58
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-09-24 00:04:07 +0300

    fixed Theme config

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -20,10 +20,12 @@ class MyApp extends StatelessWidget {
           canvasColor: Color.fromRGBO(255, 254, 229, 1),
           fontFamily: 'Raleway',
           textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              titleMedium:
-                  TextStyle(fontSize: 24, fontFamily: "RobotoCondensed"))),
+              bodyLarge: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyMedium: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              titleMedium: TextStyle(
+                  fontSize: 20,
+                  fontFamily: "RobotoCondensed",
+                  fontWeight: FontWeight.bold))),
       home: CategoriesScreen(),
     );
   }
