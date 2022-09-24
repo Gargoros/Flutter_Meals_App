17b56cd5d14e1d3f144367e02e9f7fa70f5ec8ce
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-09-24 17:05:39 +0300

    refactor: chage

--- meals_app/lib/category_meals_screen.dart
+++ meals_app/lib/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = 'category-meals';
@@ -13,14 +14,19 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs = ModalRoute.of(context)?.settings.arguments as Category;
     final categoryTitle = routeArgs.title;
     final categoryId = routeArgs.id;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle as String),
       ),
-      body: Center(
-        child: Text(
-          'The Recipes For The Category!',
-        ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
