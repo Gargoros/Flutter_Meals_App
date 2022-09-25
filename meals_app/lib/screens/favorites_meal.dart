443cd407ca203892a50ba5c5cbcd57e1dc6cc474
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-09-25 14:14:01 +0300

    init: create new Screen

---
+++ meals_app/lib/screens/favorites_meal.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FavoritesMealScreen extends StatelessWidget {
+  const FavoritesMealScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("The Favorites Meal"),
+    );
+  }
+}
